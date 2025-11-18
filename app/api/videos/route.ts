5ea6f382b4af9b786b8f340be30e26b44ebff710
Jesus12399099 <jmimportados27@gmail.com>
2025-11-18 16:33:30 -0300

    Delete app directory

--- app/api/videos/route.ts
+++
@@ -1,7 +0,0 @@
-export async function GET() {
-  return Response.json([
-    "/video1.mp4",
-    "/video2.mp4",
-    "/video3.mp4",
-  ]);
-}
--- app/page.tsx
+++
@@ -1,5 +0,0 @@
-import Feed from "./feed/Feed";
-
-export default function Page() {
-  return <Feed />;
-}
