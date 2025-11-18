538fdc6946c90fd512f61085785100f6d559f2e9
Jesus12399099 <jmimportados27@gmail.com>
2025-11-18 16:10:59 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 export const metadata = {
   title: "FLUXO",
-  description: "Aplicativo estilo TikTok feito em Next.js",
+  description: "Aplicativo estilo TikTok em Next.js",
 };
 
-import BottomBar from "./components/BottomBar";
+import BottomBar from "./feed/BottomBar";
 
 export default function RootLayout({
   children,
@@ -12,7 +12,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
-      <body style={{ margin: 0, padding: 0, background: "black" }}>
+      <body
+        style={{
+          margin: 0,
+          padding: 0,
+          background: "black",
+          overflow: "hidden",
+        }}
+      >
         {children}
         <BottomBar />
       </body>
