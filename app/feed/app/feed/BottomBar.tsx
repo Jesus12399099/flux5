97f19d8652eb3ccc37649b08ba120aad6e972dda
Jesus12399099 <jmimportados27@gmail.com>
2025-11-18 16:15:17 -0300

    Create BottomBar.tsx

---
+++ app/feed/app/feed/BottomBar.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { Home, Users, Plus, MessageCircle, User } from "lucide-react";
+
+export default function BottomBar() {
+  return (
+    <div
+      style={{
+        position: "fixed",
+        bottom: 0,
+        width: "100%",
+        height: 70,
+        background: "rgba(0,0,0,0.7)",
+        display: "flex",
+        justifyContent: "space-around",
+        alignItems: "center",
+        color: "white",
+        zIndex: 999,
+        backdropFilter: "blur(10px)",
+      }}
+    >
+      <Home size={28} />
+      <Users size={28} />
+      <Plus size={40} />
+      <MessageCircle size={28} />
+      <User size={28} />
+    </div>
+  );
+}
