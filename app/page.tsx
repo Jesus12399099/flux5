cfd959a78bc9e66cef51775eaa6adc87bb775846
Jesus12399099 <jmimportados27@gmail.com>
2025-11-18 16:11:43 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Feed from "./feed/Feed";
-export default function Home(){
-  return <main style={{background:"black",height:"100vh"}}><Feed/></main>;
+
+export default function Page() {
+  return <Feed />;
 }
