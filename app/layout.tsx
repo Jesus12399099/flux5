5fed0b7361cc98ec74f0d7b55fc1a2a58299ffc5
Jesus12399099 <jmimportados27@gmail.com>
2025-11-18 16:07:03 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ export const metadata = {
   description: "Aplicativo estilo TikTok feito em Next.js",
 };
 
+import BottomBar from "./components/BottomBar";
+
 export default function RootLayout({
   children,
 }: {
@@ -12,6 +14,7 @@ export default function RootLayout({
     <html lang="pt-br">
       <body style={{ margin: 0, padding: 0, background: "black" }}>
         {children}
+        <BottomBar />
       </body>
     </html>
   );
